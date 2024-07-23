410d52ed631f4427b09a6097466dc8f6c40d0397
BaoNgoc25 <127501275+BaoNgoc25@users.noreply.github.com>
2024-07-23 11:11:00 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://cdn.tgdd.vn/Files/2018/03/31/1078566/2aa25123-82fd-44e8-894a-a58fff098dc1_800x600.gif" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'nnnnn.bco71nx.mongodb.net',
+  DB_USER: 'nnnnn',
+  DB_PASS: 'Ltbnltbn25',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'KHOA_BI_MAT',
+  JWT_EXPIRES: '36000000000', // in milliseconds
+  EMAIL_USER: 'ngocluu.25022001@gmail.com', // gmail service
+  EMAIL_PASS: 'slxc bqyk wbnv wpap'
+};
+module.exports = MyConstants;
